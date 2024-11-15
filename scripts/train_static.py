332bec0ee9839ddaf8407e4c46ff30a3b9698981
luigi617 <luigiliu617@gmail.com>
2024-11-14 22:13:25 -0500

    train stati

--- scripts/train_static.py
+++ scripts/train_static.py
@@ -9,11 +9,45 @@ import os
 import json
 import numpy as np
 from tqdm import tqdm
-
+import random
 static = ["pointing", "open_palm", "thumb_index_touch", "fist"]
 
+def split_data(dir):
+    train_dir = {}
+    val_dir = {}
+    test_dir = {}
+    for cls in static:
+        cls_dir = os.path.join(dir, cls)
+        if not os.path.isdir(cls_dir): continue
+        json_files = [os.path.join(cls_dir, f) for f in os.listdir(cls_dir) if f.endswith('.json')]
+        random.shuffle(json_files)
+        n = len(json_files)
+        split_1 = int(0.8 * n)
+        split_2 = split_1 + int(0.1 * n)
+
+        train_dir[cls] = json_files[:split_1]
+        val_dir[cls] = json_files[split_1:split_2]
+        test_dir[cls] = json_files[split_2:]
+    
+    return train_dir, val_dir, test_dir
+
+def evaluate(model, dataloader, device):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, labels in dataloader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    accuracy = 100 * correct / total
+    return accuracy
+
+
 class StaticGestureDataset(Dataset):
-    def __init__(self, data_dir, transform=None):
+    def __init__(self, data_dir:dict, transform=None):
         """
         Args:
             data_dir (str): Path to the directory with gesture data.
@@ -22,16 +56,11 @@ class StaticGestureDataset(Dataset):
         self.data = []
         self.labels = []
         self.transform = transform
-        self.classes = static
+        self.classes = list(data_dir.keys())
         self.class_to_idx = {cls_name: idx for idx, cls_name in enumerate(self.classes)}
         
         for cls in self.classes:
-            cls_dir = os.path.join(data_dir, cls)
-            if not os.path.isdir(cls_dir):
-                continue
-            json_files = sorted([f for f in os.listdir(cls_dir) if f.endswith('.json')])
-            for jf in json_files:
-                jf_path = os.path.join(cls_dir, jf)
+            for jf_path in data_dir[cls]:
                 with open(jf_path, 'r') as f:
                     landmarks = json.load(f)
                     landmarks = preprocess_landmarks(landmarks).numpy()
@@ -52,19 +81,21 @@ class StaticGestureDataset(Dataset):
 
 def train_static_gesture_model():
     # Paths
-    train_dir = os.getcwd() + '/gesture_dataset/static/train'
-    val_dir = os.getcwd() + '/gesture_dataset/static/val'
+    static_dir = os.getcwd() + '/gesture_dataset/static'
     # Hyperparameters
     num_epochs = 50
     batch_size = 64
     learning_rate = 1e-3
     num_classes = len(static)
-    
+    train_data, val_data, test_data = split_data(static_dir)
     # Datasets and Dataloaders
-    train_dataset = StaticGestureDataset(train_dir)
-    val_dataset = StaticGestureDataset(val_dir)
+    train_dataset = StaticGestureDataset(train_data)
+    val_dataset = StaticGestureDataset(val_data)
+    test_dataset = StaticGestureDataset(test_data)
+
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=4)
     
     # Model, Loss, Optimizer
     model = StaticGestureModel(input_size=63, num_classes=num_classes).to(device)
@@ -132,6 +163,22 @@ def train_static_gesture_model():
             best_val_acc = val_epoch_acc
             torch.save(model.state_dict(), 'models/static_gesture_model.pth')
             print(f'Best model saved with Val Acc: {best_val_acc:.2f}%')
+    
+    model.load_state_dict(torch.load('models/static_gesture_model.pth'))
+    model.to(device)
+    train_acc = evaluate(model, train_loader, device)
+    
+    # Evaluate on Validation Set
+    val_acc = evaluate(model, val_loader, device)
+    
+    # Evaluate on Test Set
+    test_acc = evaluate(model, test_loader, device)
+
+    print("\nFinal Accuracies of the Best Model:")
+    print(f"Training Accuracy: {train_acc:.2f}%")
+    print(f"Validation Accuracy: {val_acc:.2f}%")
+    print(f"Test Accuracy: {test_acc:.2f}%")
+
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
