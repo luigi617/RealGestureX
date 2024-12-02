59305ec3d514c9f9d6056fa2643e78e4de9303c1
luigi617 <luigiliu617@gmail.com>
2024-12-01 19:02:10 -0500

    f

--- scripts/hand_tracking/train_hand_detection.py
+++ scripts/hand_tracking/train_hand_detection.py
@@ -94,7 +94,6 @@ def train_hand_detection(model, train_loader, val_loader, criterion, optimizer, 
                     'labels': torch.tensor([1], dtype=torch.int64).to(device)  # Hand class label
                 }
                 targets.append(target)
-            targets.to(device)
             outputs = model(images, targets)  # Predicted bounding boxes
             
             # Calculate loss (using MSE loss or other loss)
@@ -134,7 +133,7 @@ def train_hand_detection(model, train_loader, val_loader, criterion, optimizer, 
                         'labels': torch.tensor([1], dtype=torch.int64).to(device)  # Hand class label
                     }
                     targets.append(target)
-                targets.to(device)
+
                 outputs = model(images, targets)  # Predicted bounding boxes
                 loss = criterion(outputs, bboxes)
                 val_loss += loss.item()
