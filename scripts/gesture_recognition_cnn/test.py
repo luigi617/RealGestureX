1d532d86655971179eccaac3b48eefb6d102e0d0
luigi617 <luigiliu617@gmail.com>
2024-12-08 17:42:36 -0500

    f

--- scripts/gesture_recognition_cnn/test.py
+++ scripts/gesture_recognition_cnn/test.py
@@ -66,6 +66,8 @@ def plot_confusion_matrix(y_true, y_pred, class_names):
     plt.xlabel('Predicted')
     plt.ylabel('True')
     plt.show()
+    plt.savefig('cm.jpg')
+
 
 def evaluate_and_get_predictions(model, data_loader, device):
     model.eval()
