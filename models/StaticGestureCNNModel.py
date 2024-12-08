c8f4ac17f089a48f46c4b76a47bdae5945e2305b
luigi617 <luigiliu617@gmail.com>
2024-12-08 04:28:26 -0500

    fix

--- models/StaticGestureCNNModel.py
+++ models/StaticGestureCNNModel.py
@@ -48,3 +48,26 @@ class StaticGestureCNNModel(nn.Module):
         x = x.view(x.size(0), -1)  # Flatten
         x = self.classifier(x)
         return x
+    
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class StaticGestureCNNModel(nn.Module):
+    def __init__(self, num_classes=26, freeze_layers=True):
+        super(StaticGestureCNNModel, self).__init__()
+
+        self.resnet = models.resnet18(pretrained=True)
+
+        self.resnet.fc = nn.Linear(self.resnet.fc.in_features, num_classes)
+
+        if freeze_layers:
+            for param in self.resnet.parameters():
+                param.requires_grad = False 
+
+            for param in self.resnet.fc.parameters():
+                param.requires_grad = True
+
+    def forward(self, x):
+        return self.resnet(x)
+
--- scripts/gesture_recognition_cnn/train_dynamic.py
+++ scripts/gesture_recognition_cnn/train_dynamic.py
@@ -65,7 +65,7 @@ def train_dynamic_gesture_model():
 
     transform = transforms.Compose([
         transforms.ToPILImage(),
-        transforms.Resize((128, 128)), 
+        transforms.Resize((224, 224)), 
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
